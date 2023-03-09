a717f1833c53745a0b700d19e204b4cea5215004
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-03-09 16:34:43 +0300

    lowest route made protected

--- src/tasks/router.py
+++ src/tasks/router.py
@@ -8,9 +8,11 @@ router = APIRouter(prefix="/report", tags=["Reports"])
 
 
 @router.get("/lowest-temperatures")
-def get_lowest_temperatures_email(background_tasks: BackgroundTasks):
+def get_lowest_temperatures_email(
+    background_tasks: BackgroundTasks, user=Depends(current_user)
+):
     # TODO: добавить получение данных из базы, добавить async
-    background_tasks.add_task(send_email_lowest_temperatures)
+    background_tasks.add_task(send_email_lowest_temperatures, user.email)
     return {"status": 200, "data": "Email sent", "details": "with background_tasks"}
 
 
--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -9,12 +9,12 @@ SMTP_PORT = 465
 celery = Celery("tasks", broker="redis://redis:6379/0")
 
 
-def get_email_lowest_temperatures():
+def get_email_lowest_temperatures(email_to: str):
     # TODO: refactor in one function
     email = EmailMessage()
     email["Subject"] = "lowest temperatures"
     email["From"] = SMTP_USER
-    email["To"] = SMTP_USER
+    email["To"] = email_to
 
     email.set_content(
         """data""",
@@ -35,8 +35,8 @@ def get_email_highest_temperatures():
     return email
 
 
-def send_email_lowest_temperatures():
-    email = get_email_lowest_temperatures()
+def send_email_lowest_temperatures(email_to: str):
+    email = get_email_lowest_temperatures(email_to)
     with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
         server.login(SMTP_USER, SMTP_PASSWORD)
         server.send_message(email)
