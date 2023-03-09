8db589ef77b7b6cad529b32c1eeac827f590a248
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-03-09 16:05:14 +0300

    celery tasks

--- src/tasks/router.py
+++ src/tasks/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, BackgroundTasks, Depends
 
 from src.auth.base_config import current_user
 
-from .tasks import send_email_lowest_temperatures
+from .tasks import send_email_lowest_temperatures, send_email_highest_temperatures
 
 router = APIRouter(prefix="/report", tags=["Reports"])
 
@@ -11,4 +11,11 @@ router = APIRouter(prefix="/report", tags=["Reports"])
 def get_lowest_temperatures_email(background_tasks: BackgroundTasks):
     # TODO: добавить получение данных из базы, добавить async
     background_tasks.add_task(send_email_lowest_temperatures)
-    return {"status": 200, "data": "Email sent", "details": None}
+    return {"status": 200, "data": "Email sent", "details": "with background_tasks"}
+
+
+@router.get("/highest-temperatures")
+def get_highest_temperatures_email():
+    # TODO: добавить получение данных из базы, добавить async
+    send_email_highest_temperatures.delay()
+    return {"status": 200, "data": "Email sent", "details": "with celery"}
--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -1,12 +1,16 @@
 import smtplib
+from celery import Celery
 from email.message import EmailMessage
 from src.config import SMTP_PASSWORD, SMTP_USER
 
 SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = 465
 
+celery = Celery("tasks", broker="redis://redis:6379/0")
+
 
 def get_email_lowest_temperatures():
+    # TODO: refactor in one function
     email = EmailMessage()
     email["Subject"] = "lowest temperatures"
     email["From"] = SMTP_USER
@@ -18,8 +22,29 @@ def get_email_lowest_temperatures():
     return email
 
 
+def get_email_highest_temperatures():
+    # TODO: refactor in one function
+    email = EmailMessage()
+    email["Subject"] = "highest temperatures"
+    email["From"] = SMTP_USER
+    email["To"] = SMTP_USER
+
+    email.set_content(
+        """data""",
+    )
+    return email
+
+
 def send_email_lowest_temperatures():
     email = get_email_lowest_temperatures()
+    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
+        server.login(SMTP_USER, SMTP_PASSWORD)
+        server.send_message(email)
+
+
+@celery.task
+def send_email_highest_temperatures():
+    email = get_email_highest_temperatures()
     with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
         server.login(SMTP_USER, SMTP_PASSWORD)
         server.send_message(email)
